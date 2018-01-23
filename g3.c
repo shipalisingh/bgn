a2ac7f614308611843d98f3e35514accf9a7b2b6
shipalisingh <35567595+shipalisingh@users.noreply.github.com>
2018-01-24 00:03:03 +0530

    Add files via upload

---
+++ g3.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+	char n;
+	printf("enter any alphabet");
+	scanf("%c",&n);
+	if(n=='a'||n=='e'||n=='i'||n=='o'||n=='u'||n=='A'||n=='E'||n=='I'||n=='O'||n=='U')
+	printf("Vowel");
+	else
+	printf("Constant");
+	
+	
+}
