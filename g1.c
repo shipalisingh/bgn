0495ceb263b17569df957aa0e4709a0148a34f2e
shipalisingh <35567595+shipalisingh@users.noreply.github.com>
2018-01-23 23:33:34 +0530

    Add files via upload

---
+++ g1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+	int n;
+	printf("enter no");
+    scanf("%d",&n);
+    if(n>0)
+    printf("no is positive");
+    else if(n<0)
+    printf("no is negative");
+    else
+    printf("no is zero");
+	
+}
