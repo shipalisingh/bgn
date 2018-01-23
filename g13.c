183bd3c4ab9467a3e1417cca85ec81b409b7e1f8
shipalisingh <35567595+shipalisingh@users.noreply.github.com>
2018-01-24 02:55:55 +0530

    Add files via upload

---
+++ g13.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void main()
+{
+	int n,i,flag=0;
+	printf("enter a no");
+	scanf("%d",&n);
+	for(i=2;i<=n/2;i++)
+	{
+		if(n%i==0)
+		{
+			flag=1;
+			break;
+		}
+	}
+	if(flag==0)
+		printf("yes");
+		else
+		printf("no");
+	}
+	
